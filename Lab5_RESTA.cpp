4c81d45e93f341d8f055524b6bf0847b96b6ee4b
Marxbeta <99828664+Marxbeta7@users.noreply.github.com>
2022-04-04 23:38:19 -0500

    Add files via upload

---
+++ Lab5_RESTA.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+//Incluimos librerias propias de C++
+
+using namespace std;
+
+
+//En este segmento de código es necesario definir primero cómo funciona la resta pues
+//No es algo que c++ conozca 
+
+
+//RESTA 
+
+void resta(int n,int m,double A[][3],double B[][3],double D[][3]){
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			D[i][j]=A[i][j]-B[i][j];
+}
+
+
+//Se implementa la función imprimir para poder visualizar el resultado 
+
+void imprimir(int n, int m, double D[][3]){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++)
+			cout<<D[i][j]<<" ";
+		cout<<endl;
+	}
+}
+
+//Ahora si inicializamos las variables para probar que 
+//Las funciones operan correctamenten 
+
+
+int main(){
+    
+    //Con n filas y m columnas 
+    
+	int n=2;
+	int m=3;
+	
+	double A[n][3]={{1,2,3},{4,5,6}};
+	double B[n][3]={{6,5,4},{3,2,1}};
+
+
+	//OUTPUT
+	double D[n][3];
+	
+	//vISUALIZACIÓND DE LAS DOS MATRICES 
+	
+	cout<<"A=";
+	imprimir(n,m,A);
+	cout<<"B=";
+	imprimir(n,m,B);
+	
+	
+	//Mostrar el resultado de la suma 
+	
+	cout<<"Resta de A y B"<<endl;
+	resta(n,m,A,B,D);
+	imprimir(n,m,D);
+
+	return 0;
+}
